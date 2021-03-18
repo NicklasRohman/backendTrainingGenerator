84e38e707958edcc31d9211f839d9723ffc04da2
NicklasRohman <nicklasrohman@gmail.com>
2021-03-18 11:17:52 +0100

    change in entity

--- src/main/java/se/nicklasrohman/backendTrainingGenerator/entity/Exercises.java
+++ src/main/java/se/nicklasrohman/backendTrainingGenerator/entity/Exercises.java
@@ -2,18 +2,24 @@ package se.nicklasrohman.backendTrainingGenerator.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "training")
 public class Exercises {
 
     @Id
+    @Column(name = "exercise_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     int exerciseId;
+    @Column(name = "exercise_name")
     String exerciseName;
+    @Column(name = "difficult_level")
     int difficultLevel;
+    @Column(name = "estimated_time")
     double estimatedTime;
+    @Column(name = "video_path")
     String videoPath;
 
 }
