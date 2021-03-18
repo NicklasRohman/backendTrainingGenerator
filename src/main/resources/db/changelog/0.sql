f04a10853b5395c38b80e1aea7dd098ef96e3393
NicklasRohman <nicklasrohman@gmail.com>
2021-03-18 09:53:42 +0100

    change sql

--- src/main/resources/db/changelog/0.sql
+++ src/main/resources/db/changelog/0.sql
@@ -2,26 +2,48 @@
 
 --changeset nicklas.rohman:1 logicalFilePath:0.sql
 --comment: adding training table
-CREATE TABLE training_database.training(
-    id int UNSIGNED AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS training_database.training(
+    exercise_id int UNSIGNED AUTO_INCREMENT NOT NULL,
     exercise_name varchar(255) NOT NULL,
-    image_path varchar(255),
+    difficult_level int(10) NOT NULL DEFAULT 1,
+    estimated_time double,
     video_path varchar(255),
-    PRIMARY KEY (id)
+    PRIMARY KEY (exercise_id)
 )
+engine=innodb;
 
 --changeset nicklas.rohman:2 logicalFilePath:0.sql
---comment: adding training table
-CREATE TABLE training_database.category(
-    id int UNSIGNED AUTO_INCREMENT NOT NULL,
+--comment: adding category table
+CREATE TABLE IF NOT EXISTS training_database.category(
+    id int UNSIGNED  NOT NULL,
+    exercise_id int UNSIGNED NOT NULL,
     category_name varchar(255) NOT NULL,
-    PRIMARY KEY (id)
+    CONSTRAINT FK_CategoryTraining FOREIGN KEY (exercise_id) REFERENCES training(exercise_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 )
+engine=innodb;
 
 --changeset nicklas.rohman:3 logicalFilePath:0.sql
---comment: adding training table
-CREATE TABLE training_database.tag(
-    id int UNSIGNED AUTO_INCREMENT NOT NULL,
-    tag_name varchar(255) NOT NULL,
-    PRIMARY KEY (id)
+--comment: adding style table
+CREATE TABLE IF NOT EXISTS training_database.style(
+    id int UNSIGNED  NOT NULL,
+    exercise_id int UNSIGNED NOT NULL,
+    style_name varchar(255) NOT NULL,
+    CONSTRAINT FK_StyleTraining FOREIGN KEY (exercise_id) REFERENCES training(exercise_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
 )
+engine=innodb;
+
+--changeset nicklas.rohman:4 logicalFilePath:0.sql
+--comment: adding image table
+CREATE TABLE IF NOT EXISTS training_database.image(
+    id int UNSIGNED NOT NULL,
+    exercise_id int UNSIGNED NOT NULL,
+    image_name varchar(255) NOT NULL,
+    CONSTRAINT FK_ImageTraining FOREIGN KEY (exercise_id) REFERENCES training(exercise_id)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+)
+engine=innodb;
