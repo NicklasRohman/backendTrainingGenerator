ec4ec1101cbd8cd2eddada050f92435c529bb673
NicklasRohman <nicklasrohman@gmail.com>
2021-03-25 10:36:29 +0100

    Move up exercise rto requestMapping

--- src/main/java/se/nicklasrohman/backendTrainingGenerator/controller/ExercisesController.java
+++ src/main/java/se/nicklasrohman/backendTrainingGenerator/controller/ExercisesController.java
@@ -4,33 +4,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.nicklasrohman.backendTrainingGenerator.dto.ExerciseDto;
 
+@RequestMapping("/exercise")
 public interface ExercisesController {
 
-    @GetMapping("/exercises")
+    @GetMapping("/all")
     ResponseEntity<Object> getAllExercises();
 
-    @GetMapping("/exercise/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<ExerciseDto> getExerciseById(int id);
 
-    @PostMapping("/exercise")
+    @PostMapping("/create")
     ResponseEntity<Object> addNewExercise(
             @RequestParam("exerciseName") String exerciseName,
             @RequestParam("difficultLevel") int difficultLevel,
             @RequestParam("estimatedTime") double estimatedTime,
             @RequestParam("videoPath") String videoPath);
 
-    @PutMapping("/exercise/{id}")
+    @PutMapping("/update/{id}")
     ResponseEntity<Object> updateExercise(
             @PathVariable int id,
             @RequestParam("exerciseName") String exerciseName,
             @RequestParam("difficultLevel") int difficultLevel,
             @RequestParam("estimatedTime") double estimatedTime,
-            @RequestParam("videoPath") String videoPath
+            @RequestParam("videoPath") String videoPath);
 
-    );
-
-
-    @DeleteMapping("/exercise/{id}")
+    @DeleteMapping("/delete/{id}")
     ResponseEntity<Object> deleteExercise(@PathVariable int id);
 
 }
