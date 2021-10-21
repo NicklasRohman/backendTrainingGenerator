70c3c103749599957f025aabd392daf3340a2b04
NicklasRohman <nicklasrohman@gmail.com>
2021-10-21 18:03:59 +0200

    update tag entity

--- src/main/java/se/nicklasrohman/backendTrainingGenerator/entity/Tags.java
+++ src/main/java/se/nicklasrohman/backendTrainingGenerator/entity/Tags.java
@@ -1,6 +1,19 @@
 package se.nicklasrohman.backendTrainingGenerator.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="tags")
 public class Tags {
+
+    @Id
+    @Column(name = "tag_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     int tagId;
+
+    @Column(name = "tag_name")
     String tagName;
 }
