aa16df4c1696d7cca650d7f7a29455d5b31021d1
NicklasRohman <nicklasrohman@gmail.com>
2021-10-30 21:47:02 +0200

    added TagRepository file

---
+++ src/main/java/se/nicklasrohman/backendTrainingGenerator/repository/init/TagsRepositoryCustomInit.java
@@ -0,0 +1,10 @@
+package se.nicklasrohman.backendTrainingGenerator.repository.init;
+
+import org.springframework.stereotype.Repository;
+import se.nicklasrohman.backendTrainingGenerator.repository.TagsRepositoryCustom;
+
+@Repository
+public class TagsRepositoryCustomInit implements TagsRepositoryCustom {
+
+
+}
